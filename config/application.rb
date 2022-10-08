fb1590631fe75ef05489d224e8c64daadf5e08ba
Adarsh <m0400@Adarshs-MacBook-Pro.local>
2022-10-09 01:07:38 +0530

    Final touch

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module ScheduledTweets
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    # config.time_zone = 'Kolkata'
     config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,28 @@ Rails.application.routes.draw do
 
   get "about", to: "about#index"
 
+  get "password", to: "passwords#edit", as: :edit_password
+  patch "password", to: "passwords#update"
+
+  post "sign_up", to: "registration#create"
+  get "sign_up", to:"registration#new"
+
+  get "sign_in", to:"sessions#new"
+  post "sign_in", to: "sessions#create"
+
+  delete "logout", to:"sessions#destroy"
+
+  get "password/reset", to:"password_resets#new"
+  post "password/reset", to:"password_resets#create"
+
+  get "password/reset/edit", to:"password_resets#edit"
+  patch "password/reset/edit", to:"password_resets#update"
+
+  get "auth/twitter/callback", to: "omniauth_callbacks#twitter"
+
+  resources :twitter_accounts
+  resources :tweets
+
   # get "/", to: "main#index"
   root to: "main#index"
 end
